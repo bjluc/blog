7a6aef18bc8694d0ef88ffff0160f29607c8ab0f
bjluc <bjluc@bjluc.co.uk>
2017-05-15 20:27:59 +0100

    adding seach facility

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -43,4 +43,30 @@ class FrontEndController extends Controller
 
    }
 
+   public function category($id)
+
+   {
+
+      $category = Category::find($id);
+ 
+      return view('category')->with('category', $category)
+                             ->with('title', $category->name)
+                             ->with('settings', Setting::first())
+                             ->with('categories', Category::take(5)->get());
+
+   }
+
+   public function tag($id)
+
+   {
+
+      $tag = Tag::find($id);
+ 
+      return view('tag')->with('tag', $tag)
+                        ->with('title', $tag->tag)
+                        ->with('settings', Setting::first())
+                        ->with('categories', Category::take(5)->get());
+
+   }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,16 @@
 
 //Front End Route
 Route::get('/', ['uses' => 'FrontEndController@index', 'as' => 'index']);
+Route::get('/results', function(){$posts = App\Post::where('title','like','%'.request('query').'%')->get();
+return view('results')->with('posts', $posts)
+                      ->with('title', 'Search results: '.request('query'))
+                      ->with('settings', App\Setting::first())
+                      ->with('categories', App\Category::take(5)->get())
+                      ->with('query', request('query'));
+});
 Route::get('/post/{slug}', ['uses' => 'FrontEndController@singlePost', 'as' => 'post.single']);
+Route::get('/category/{id}', ['uses' => 'FrontEndController@category', 'as' => 'category.single']);
+Route::get('/tag/{id}', ['uses' => 'FrontEndController@tag', 'as' => 'tag.single']);
 
 
 //Back End Route
